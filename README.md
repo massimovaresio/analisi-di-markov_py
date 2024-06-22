0b33eb134d1fa063a38070a3181578de75b89405
massimovaresio <varesio.massimo@gmail.com>
2024-06-22 12:24:31 +0200

    Progetto Markov completo e funzionante

--- README.md
+++ README.md
@@ -6,5 +6,17 @@ In questa esercitazione scriverò un programma che legge un file di testo ed ese
 
 Il file di testo che verrà processato e analizzato è relativo al romanzo *Emma* della scrittrice [Jane Austen](https://it.wikipedia.org/wiki/Jane_Austen). Il file fa riferimento al noto [Progetto Gutenberg](https://it.wikipedia.org/wiki/Progetto_Gutenberg) di e-book open source che tuttavia, alla data del presente progetto, sembra irrangiungibile dal browser forse a causa di un errore nel protocollo http. Pertanto ho scaricato il file `emma11.txt` dalle risorse libere di [Interne Archive](https://archive.org/details/emma00158gut); si prega di rispettare i termini di licenza e riuso previsti dal sito.
 
+### Sintesi del codice
+Le prime funzioni definite `leggi_pulisci_testo()`, `taglia_testo()`, `elabora_riga()` sono preposte ad aprire il file di testo cercando di gestire semplici casi di errore, rimuovere le parti di testo superflue ed elaborare il testo affinché venga convertito in una lista di parole.
+Segue la funzione `costruisci_catena_markov()` che prende questa lista di parole e un parametro relativo alla lunghezza stabilita per i prefissi, e costruisce un dizionario in cui le chiavi sono i prefissi della lunghezza stabilita dall'utente e i valori sono rappresentati dai possibili suffissi che seguono i prefissi.
+Infine, ho aggiunto una funzione `genera_testo_casuale()` che sfruttando le pontenzialità del modulo Python `random` crea un testo casuale basato sull'analisi di Markov.
+Per una spiegazione dettagliata di come opera il codice si rinvia ai commenti interni, forse addirittura sovrabbondanti! :dizzy_face: Scusatemi al riguardo, ma ho sviluppato il progetto a scopo didattico per principianti, quale sono io :sweat_smile:
+
+### Considerazioni finali
+Lo scopo finale di questo progetto era infine capire cosa succede se si aumenta la lunghezza del prefisso; il testo sarà sintatticamente e semanticamente più o meno sensato? Dopo aver effettuato alcuni testi confermo che sì, più il prefisso è lungo più il testo generato sembra di senso compiuto. Tuttavia ho limitato il numero di parole per prefisso a 9, altrimenti andando oltre l'esperimento perde di significato in quando i prefissi stessi diventavano già frasi di senso compiuto, ovviamente. Questo avviene perché un prefisso più lungo consente al modello di catturare relazioni più specifiche tra le parole nel testo originale. 
+In conclusione, l'aumento della lunghezza del prefisso nella tua implementazione della catena di Markov può sicuramente migliorare la qualità semantica e logica del testo generato.
+**Piccolo bug**: nei casi in cui la lunghezza del prefisso è bassa, 2 o 3 parole, è possibile che alcuni suffissi siano più di una singola parola. In questo modo può succedere che andando via via ad aggiungere i suffissi al testo generato, lo stesso testo risulti di qualche parola più lungo rispetto al valore inserito dall'utente. Tuttavia ai fini dell'esperimento mi è sembrato un bug tollerabile; se qualcuno ha suggerimenti su come migliorare è benvenuto :blush:
+
+
 ### Licenza
 Questo progetto è liberamente utilizzabile e replicabile, nel rispetto dei termini della licenza [CC 4.0](https://creativecommons.org/licenses/by/4.0/).
--- analisiMarkov.py
+++ analisiMarkov.py
@@ -1,5 +1,6 @@
 import string  # modulo string di python https://docs.python.org/3/library/string.html
 from collections import defaultdict
+import random
 
 
 def leggi_pulisci_testo(nome_file):
@@ -76,6 +77,31 @@ def costruisci_catena_markov(lista_parole, lunghezza_prefisso=2):
     return dict(catena_markov)  # dict() converte in un dizionario ordinario
 
 
+def genera_testo_casuale(catena_markov, lunghezza_testo, lunghezza_prefisso=2):
+    """Genera un testo casuale basato sulla catena di Markov data."""
+    # la funzione random.choice necessita di una sequenza come argomento, tipicamente una lista o una tupla. Il medoto .keys() restituisce una vista delle chiavi 
+    # del dizionario e le viste non supportano l'indicizzazione diretta, quindi random.choice non può lavorarci direttamente,
+    # pertanto occorre convertire questa vista in una vera lista
+    prefisso_iniziale = random.choice(list(catena_markov.keys()))  #  sceglie casualmente un prefisso iniziale
+
+    # random.choice(list(catena_markov.keys())) restituisce una tupla, non una lista. 
+    # La conversione in lista con list(prefisso_iniziale) è necessaria per poter costruire il testo_generato come una lista di parole
+    testo_generato = list(prefisso_iniziale)
+
+    # questo ciclo si ripete lunghezza_testo - lunghezza_prefisso volte per generare il testo. L'uso di - lunghezza_prefisso è dovuto al fatto che il testo_generato 
+    # è inizialmente popolato con il prefisso_iniziale, che ha già una lunghezza pari a lunghezza_prefisso.
+    for _ in range(lunghezza_testo - lunghezza_prefisso):
+        suffissi_possibili = catena_markov.get(prefisso_iniziale)  # suffissi_possibili è una lista, non una singola stringa; il valore associato a una chiave nel dizionario catena_markov è sempre una lista di possibili suffissi, quindi catena_markov.get(prefisso_iniziale) restituisce una lista
+        if not suffissi_possibili:  # se non ci sono più suffissi disponibili, termina il ciclo
+            break
+        prossimo_suffisso = random.choice(suffissi_possibili)
+        testo_generato.append(prossimo_suffisso)
+        prefisso_iniziale = tuple(testo_generato[-lunghezza_prefisso:])  # aggiorna il prefeisso: testo_generato[-lunghezza_prefisso:] significa: "Prendi gli ultimi 'lunghezza_prefisso' elementi dalla lista 'testo_generato'"; 
+                                                                         # ad esempio 'testo_generato[-2:]' prende gli ultimi due elementi della lista testo_generato; queso viene convertiro in tupla perché occorre riaccedere ai prefissi ossia alle chiavi del dizionario che sono tuple
+
+    return ' '.join(testo_generato)  # ' '.join(testo_generato) combina gli elementi della lista testo_generato in una singola stringa separata da spazi; questo serve per rendere il testo generato leggibile come unica stringa
+
+
 
 
 #ESECUZIONE
@@ -85,10 +111,19 @@ if __name__ == "__main__":
     # uso l'espressione bool 'if testo_pulito:' per controllare la veridicità di una variabile; controlla che la variabile non sia 'None' o da altri valori "falsi", come ad esempio liste vuote; 
     # questo approccio è in linea con le convenzioni Python e rende il codice più chiaro e leggibile
     if testo_pulito:
-        lunghezza_prefisso = 2
-        catena_markov = costruisci_catena_markov(testo_pulito, lunghezza_prefisso)
+        lunghezza_prefisso = int(input("Inserisci la lunghezza del prefisso (consigliato da 2 a 9): "))
 
-        for chiave, valore in catena_markov.items():
-            print(f"{chiave}: {valore}")
+        if lunghezza_prefisso <= 9:
+            catena_markov = costruisci_catena_markov(testo_pulito, lunghezza_prefisso)
+
+            for chiave, valore in catena_markov.items():
+                print(f"{chiave}: {valore}")
+
+            lunghezza_testo = int(input("\nInserisci la lunghezza del testo desiderata (consigliato non più di 1000 parole): "))
+            testo_generato = genera_testo_casuale(catena_markov, lunghezza_testo, lunghezza_prefisso)
+            print("\nTesto generato:")
+            print(testo_generato)
+        else:
+            print("Il prefisso inserito è troppo lungo e compromette il risultato dell'analisi.")        
     else:
-        print("Il testo è vuoto o non è stato letto correttamente.")     
+        print("Il testo è vuoto o non è stato letto correttamente.")  
